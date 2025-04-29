d6d47cf7d0c162d0c1530f98724a51d627ccbe49
Santiago <santtyconla@gmail.com>
2025-04-29 08:48:40 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConlagoS_PruebaProgeso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
