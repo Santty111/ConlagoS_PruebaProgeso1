4075b246a4ebf401d0dba5f2f31ebb118f48dba3
Santiago <santtyconla@gmail.com>
2025-04-29 15:55:45 -0500

    eliminacion de comentarios

--- Models/PlanRecompensa.cs
+++ Models/PlanRecompensa.cs
@@ -6,20 +6,20 @@ namespace ConlagoS_PruebaProgeso1.Models
     public class PlanRecompensa
     {
         [Key]
-        public int Id { get; set; } // ID del plan
+        public int Id { get; set; } 
 
         [Required(ErrorMessage = "El nombre del plan es obligatorio.")]
         [StringLength(100, ErrorMessage = "El nombre no debe superar los 100 caracteres.")]
-        public string Nombre { get; set; } // Nombre del plan
+        public string Nombre { get; set; } 
 
         [Required(ErrorMessage = "Debe ingresar la fecha de inicio.")]
         [DataType(DataType.Date)]
         [Display(Name = "Fecha de Inicio")]
-        public DateTime FechaInicio { get; set; } // Fecha en que inició el plan
+        public DateTime FechaInicio { get; set; } 
 
         [Range(0, int.MaxValue, ErrorMessage = "Los puntos acumulados deben ser positivos.")]
         [Display(Name = "Puntos Acumulados")]
-        public int PuntosAcumulados { get; set; } // Se suman 100 por cada reserva realizada
+        public int PuntosAcumulados { get; set; } 
 
         [NotMapped]
         [Display(Name = "Tipo de Recompensa")]
@@ -31,7 +31,7 @@ namespace ConlagoS_PruebaProgeso1.Models
             }
         }
 
-        // Relación: Cada cliente tiene un plan de recompensas
+       
         public int ClienteId { get; set; }
         public virtual Cliente Cliente { get; set; }
     }
